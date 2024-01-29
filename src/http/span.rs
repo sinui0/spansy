09d2bbf2bbff1caa6277c86bf949fa00025c7023
sinu <65924192+sinui0@users.noreply.github.com>
2024-01-29 14:39:13 -0800

    add subspan methods to types

--- src/http/span.rs
+++ src/http/span.rs
@@ -86,7 +86,9 @@ pub(crate) fn parse_request_from_bytes(src: &Bytes, offset: usize) -> Result<Req
 
         request.span = Span::new_bytes(src.clone(), offset..range.end);
 
-        request.body = Some(Body(Span::new_bytes(src.clone(), range)));
+        request.body = Some(Body {
+            span: Span::new_bytes(src.clone(), range),
+        });
     }
 
     Ok(request)
@@ -172,7 +174,9 @@ pub(crate) fn parse_response_from_bytes(
 
         response.span = Span::new_bytes(src.clone(), offset..range.end);
 
-        response.body = Some(Body(Span::new_bytes(src.clone(), range)));
+        response.body = Some(Body {
+            span: Span::new_bytes(src.clone(), range),
+        });
     }
 
     Ok(response)
